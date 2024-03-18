22a028afc7c5e275e8acb90610296fd3eb6e76b2
Piotr Skowron <skowronpiotrmail@gmail.com>
2024-03-18 18:42:42 +0100

    Movie model

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MovieData(models.Model):
+    name = models.CharField(max_length=200)
+    rating = models.FloatField()
+    duration = models.FloatField()
+
+    def __str__(self):
+        return self.name
